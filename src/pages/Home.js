688d8daebebb15e9446d37bcb01eb198c55a94bc
Oneill <yujinoneill@gmail.com>
2022-05-24 21:29:09 +0900

    FEAT:Add styled-components, Monthly plan, Weekly plan

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,6 @@ import styled from "styled-components";
 import BasicBox from "../components/BasicBox";
 import CheckForm from "../components/CheckForm";
 import Chart from "../components/chart/Chart";
-const Container = styled.div`
-  display: flex;
-
-  .annual-plan,
-  .monthly-plan {
-    width: 50%;
-  }
-`;
 
 const WelcomeContent = styled.div`
   display: flex;
@@ -23,6 +15,46 @@ const WelcomeContent = styled.div`
   img {
     height: 140px;
     margin: auto;
+  }
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 1fr;
+
+  box-sizing: border-box;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+const AnnualPlan = styled.div`
+  padding-right: 5%;
+  border-right: 1px solid #e7ecef;
+
+  @media screen and (max-width: 768px) {
+    border-right: none;
+    border-bottom: 1px solid #e7ecef;
+  }
+`;
+
+const MonthlyPlan = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+const WeeklyPlan = styled.div`
+  padding-left: 5%;
+  border-left: 1px solid #e7ecef;
+
+  @media screen and (max-width: 768px) {
+    border-left: none;
+    border-top: 1px solid #e7ecef;
   }
 `;
 
@@ -73,7 +105,7 @@ const Home = () => {
         boxTitle={"Wonderful 2022"}
         boxContent={
           <Container>
-            <div className="annual-plan">
+            <AnnualPlan>
               <div className="check-box">
                 <CheckForm label={"Lose weight"} />
                 <CheckForm label={"Make exercise a habbit"} />
@@ -87,10 +119,32 @@ const Home = () => {
                 </div>
                 <Progress value="70" max="100" />
               </ProgressBox>
-            </div>
-            <div className="monthly-plan">
+            </AnnualPlan>
+            <MonthlyPlan>
+              <select>
+                <option value="may">May</option>
+                <option value="april">April</option>
+                <option value="march">March</option>
+                <option value="february">Febraury</option>
+                <option value="january">January</option>
+              </select>
               <Chart />
-            </div>
+            </MonthlyPlan>
+            <WeeklyPlan>
+              <select>
+                <option value="Week 1">Week 1</option>
+                <option value="Week 2">Week 2</option>
+                <option value="Week 3">Week 3</option>
+                <option value="Week 4">Week 4</option>
+                <option value="Week 5">Week 5</option>
+              </select>
+              <div>
+                <CheckForm label={"Read 「The Stranger」"} />
+                <CheckForm label={"Go to cinema"} />
+                <CheckForm label={"Read 「1984」"} />
+                <CheckForm label={"Finish the Project"} />
+              </div>
+            </WeeklyPlan>
           </Container>
         }
       />
