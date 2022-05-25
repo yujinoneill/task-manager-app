675184a201e9c1f9180360520512c067d2c2e829
Oneill <yujinoneill@gmail.com>
2022-05-25 21:18:40 +0900

    FEAT:Add props to Styled-components

--- src/components/BasicBox.js
+++ src/components/BasicBox.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const Section = styled.section`
+const StyledSection = styled.section`
   background-color: white;
   box-shadow: 0 10px 15px rgba(0, 0, 0, 0.15);
   box-sizing: border-box;
@@ -8,9 +8,11 @@ const Section = styled.section`
   font-family: "Ubuntu", sans-serif;
   color: #274c77;
 
-  padding: 20px;
+  padding: ${(props) => props.padding || "20px"};
   margin-top: 20px;
   border-radius: 5px;
+
+  overflow: hidden;
 
   h3 {
     margin: 0;
@@ -18,19 +20,19 @@ const Section = styled.section`
   }
 
   div {
-    margin-top: 10px;
+    margin-top: ${(props) => props.marginTop || "10px"};
     margin-bottom: 10px;
   }
 `;
 
-const BasicBox = ({ boxTitle, boxContent }) => {
+const BasicBox = ({ boxTitle, boxContent, padding, marginTop }) => {
   return (
-    <Section>
+    <StyledSection padding={padding} marginTop={marginTop}>
       <header>
         <h3>{boxTitle}</h3>
       </header>
       <div>{boxContent}</div>
-    </Section>
+    </StyledSection>
   );
 };
 
