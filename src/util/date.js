9b4c09c441d570426d35c0b283d115123bde2000
Oneill <yujinoneill@gmail.com>
2022-06-01 12:12:25 +0900

    initial commit

---
+++ src/util/date.js
@@ -0,0 +1,7 @@
+export const getStringDate = (date) => {
+  let year = date.getFullYear();
+  let month = String(date.getMonth() + 1).padStart(2, "0"); //padStart는 문자열에 적용가능하므로 String() 이용해서 문자열로 변환
+  let day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+};
