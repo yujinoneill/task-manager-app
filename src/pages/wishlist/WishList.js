b9a10d332cb97cdbf6a85803866f2934ed5c4fcb
Oneill <yujinoneill@gmail.com>
2022-06-02 12:03:55 +0900

    FEAT: Add FilteredList and temporary data

--- src/pages/wishlist/WishList.js
+++ src/pages/wishlist/WishList.js
@@ -1,10 +1,17 @@
+import { useContext } from "react";
+import { DiaryStateContext } from "../../App";
 import BasicBox from "../../components/BasicBox";
-import WishBox from "../../components/wishlist/WishBox";
+import FilteredList from "../../components/FilteredList";
 
 const WishList = () => {
+  const diaryList = useContext(DiaryStateContext);
+
   return (
     <div>
-      <BasicBox boxTitle={"Wish List"} boxContent={<WishBox />} />
+      <BasicBox
+        boxTitle={"Wish List"}
+        boxContent={<FilteredList type="wish" list={diaryList} />}
+      />
     </div>
   );
 };
