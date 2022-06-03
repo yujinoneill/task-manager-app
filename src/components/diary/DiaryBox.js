cc193649e16b5757f7d3322e5b43d3ab23cc7e27
Oneill <yujinoneill@gmail.com>
2022-06-03 21:53:48 +0900

    FEAT: Add margin, Add CategoryBadge, Add some props, Pass targetDiary

--- src/components/diary/DiaryBox.js
+++ src/components/diary/DiaryBox.js
@@ -1,11 +1,17 @@
+import { useContext, useState } from "react";
 import styled from "styled-components";
+import { DiaryStateContext } from "../../App";
+
 import { getStringDate } from "../../util/date";
 import Modal from "../Modal";
+import DiaryDetail from "./DiaryDetail";
 
 const StyledBox = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
+
+  margin: 10px 0;
 
   word-wrap: break-word;
 
@@ -52,6 +58,18 @@ const DiaryBody = styled.div`
   }
 `;
 
+const CategoryBadge = styled.span`
+  display: inline-block;
+  padding: 5px 10px;
+  margin-bottom: 10px;
+
+  border-radius: 20px;
+  background-color: ${(props) =>
+    props.category === "Study" ? "#007bff" : "#ffb400"};
+
+  color: white;
+`;
+
 // Images
 const images = [
   "clement-helardot-95YRwf6CNw8-unsplash",
@@ -61,29 +79,39 @@ const images = [
   "juanjo-jaramillo-mZnx9429i94-unsplash",
 ];
 
-const DiaryBox = ({ title, content, date }) => {
+const DiaryBox = ({ id, title, content, date, category }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
 
+  const diaryList = useContext(DiaryStateContext);
+
+  const targetDiary = diaryList.find(
+    (item) => parseInt(item.id) === parseInt(id)
+  );
+
   const modalHandler = () => setIsModalVisible(!isModalVisible);
+
   return (
-    <StyledBox>
+    <div>
       <StyledBox onClick={modalHandler}>
-        src={process.env.PUBLIC_URL + `assets/${images[randomIdx]}.jpg`}
-        alt="diary-thumbnail"
-      />
-      <DiaryBody>
-        <h5>{title}</h5>
-        <p className="diary-content">{content}</p>
-        <hr />
-        <p className="post-date">{getStringDate(new Date(date))}</p>
-      </DiaryBody>
-    </StyledBox>
+        <img
+          src={process.env.PUBLIC_URL + `assets/${images[id]}.jpg`}
+          alt="diary-thumbnail"
+        />
+        <DiaryBody>
+          <CategoryBadge category={category}>{category}</CategoryBadge>
+          <h5>{title}</h5>
+          <p className="diary-content">{content}</p>
+          <hr />
+          <p className="post-date">{getStringDate(new Date(date))}</p>
+        </DiaryBody>
+      </StyledBox>
       {isModalVisible && (
         <Modal
           modalHandler={modalHandler}
           children={<DiaryDetail {...targetDiary} />}
         /> //props로 함수 내려줘서 자식 컴포넌트에서도 같은 함수 사용하기
       )}
+    </div>
   );
 };
 
