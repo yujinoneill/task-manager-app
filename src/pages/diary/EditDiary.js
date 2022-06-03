ac425bd2098ed181f4ab4118a17812c097f6f1d1
Oneill <yujinoneill@gmail.com>
2022-06-03 09:59:10 +0900

    FEAT: Make targetDiary and update originData, Pass originData

--- src/pages/diary/EditDiary.js
+++ src/pages/diary/EditDiary.js
@@ -1,7 +1,39 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../../App";
 import DiaryEditor from "../../components/diary/DiaryEditor";
 
 const EditDiary = () => {
-  return <DiaryEditor boxTitle="Edit Diary" />;
+  const [originData, setOriginData] = useState();
+  const { id } = useParams();
+  const diaryList = useContext(DiaryStateContext);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (diaryList.length > 0) {
+      const targetDiary = diaryList.find(
+        (item) => parseInt(item.id) === parseInt(id)
+      );
+
+      //targetDiary가 있을 때만
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else {
+        navigate("/diary", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+
+  return (
+    originData && (
+      <DiaryEditor
+        boxTitle="Edit Diary"
+        isEdit="true"
+        originData={originData}
+      />
+    )
+  );
 };
 
 export default EditDiary;
