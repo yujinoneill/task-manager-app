ece884eb1053342e6377d3b644e29eb12820ef24
Oneill <yujinoneill@gmail.com>
2022-06-07 15:01:05 +0900

    FEAT: Remove Dimmer, Add some style and useEffect to prevent scrolling
    Prevent scrolling when the modal is visible

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,20 +1,7 @@
+import { useEffect } from "react";
 import styled from "styled-components";
 
 //Styled-components
-const Dimmer = styled.div`
-  box-sizing: border-box;
-
-  position: fixed;
-
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-
-  background-color: rgba(0, 0, 0, 0.6);
-  z-index: 999;
-`;
-
 const ModalWrapper = styled.div`
   box-sizing: border-box;
 
@@ -25,9 +12,10 @@ const ModalWrapper = styled.div`
   left: 0;
   right: 0;
 
-  z-index: 1000;
+  z-index: 999;
 
-  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.6);
+
   outline: 0;
 `;
 
@@ -38,10 +26,13 @@ const ModalContent = styled.div`
 
   background-color: white;
   border-radius: 5px;
-  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.5);
+  box-shadow: 0 10px 15px rgb(0, 0, 0, 0.15);
 
   width: 60%;
   min-width: 300px;
+  max-height: 80%;
+
+  overflow: auto;
 
   top: 50%;
   transform: translateY(-50%);
@@ -57,20 +48,24 @@ const ModalContent = styled.div`
   }
 `;
 
-const Modal = ({ children, modalHandler, width }) => {
+const Modal = ({ children, modalHandler }) => {
   const preventPropagation = (e) => {
     e.stopPropagation();
   };
 
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "visible";
+    };
+  }, []);
+
   return (
-    <div>
-      <Dimmer />
-      <ModalWrapper tabIndex="-1" onClick={modalHandler}>
-        <ModalContent tabIndex="0" onClick={preventPropagation}>
-          {children}
-        </ModalContent>
-      </ModalWrapper>
-    </div>
+    <ModalWrapper tabIndex="-1" onClick={modalHandler}>
+      <ModalContent tabIndex="0" onClick={preventPropagation}>
+        {children}
+      </ModalContent>
+    </ModalWrapper>
   );
 };
 
