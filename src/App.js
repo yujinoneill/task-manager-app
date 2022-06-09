37def13e07fd9d1ba0b8be7bdb0ab3b92ce36908
Oneill <yujinoneill@gmail.com>
2022-06-09 14:59:25 +0900

    FEAT: Relocate diaryReducer and dataId

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import styled from "styled-components";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+
+import styled from "styled-components";
 
 import "./App.css";
 import { diaryActions } from "./store/diary";
 
 // components
 import Header from "./components/Header";
+import SideBar from "./components/SideBar";
 
 // pages
 import Home from "./pages/Home";
@@ -18,8 +21,6 @@ import WishList from "./pages/wishlist/WishList";
 import NewWish from "./pages/wishlist/NewWish";
 import EditWish from "./pages/wishlist/EditWish";
 
-import SideBar from "./components/SideBar";
-
 // styled-components
 const StyledApp = styled.div`
   padding: 20px;
@@ -30,92 +31,19 @@ const StyledApp = styled.div`
 //Function & Data
 export const DiaryDispatchContext = React.createContext();
 
-const diaryReducer = (state, action) => {
-  let newState = [];
-
-  switch (action.type) {
-    case "INIT": {
-      return action.data;
-    }
-    case "CREATE": {
-      newState = [action.data, ...state];
-      break;
-    }
-    case "EDIT": {
-      newState = state.map((item) =>
-        item.id === action.data.id ? action.data : item
-      );
-      break;
-    }
-    case "REMOVE": {
-      newState = state.filter((item) => item.id !== action.targetId);
-      break;
-    }
-    default: {
-      return state;
-    }
-  }
-
-  localStorage.setItem("diary", JSON.stringify(newState));
-  return newState;
-};
-
 function App() {
   const dispatch = useDispatch();
-  const dataId = useRef(0);
 
   useEffect(() => {
-    const localData = localStorage.getItem("diary");
-    // const localData = localStorage.length ? localStorage.getItem("diary") : {};
+    const localData = localStorage.getItem("data");
 
-    //if(localData.length > 2)
-    if (localData && localData.length > 2) {
-      const localDiaryList = JSON.parse(localData); //localData 직렬화
-      const diaryListLastData = localDiaryList.sort(
-        (a, b) => parseInt(b.id) - parseInt(a.id)
-      ); // 내림차순으로 정렬
-      dataId.current = parseInt(diaryListLastData[0].id) + 1; //단순히 diaryList의 길이를 기준으로 current값을 변경하면 삭제된 다이어리가 있을 때 id가 겹칠 수 있음
+    // && localData.length > 21
 
-      dispatch({ type: "INIT", data: localDiaryList });
+    if (localData) {
+      const localDiaryList = JSON.parse(localData).diary; //localData 직렬화
+      dispatch(diaryActions.diaryInit(localDiaryList));
     }
   }, []);
-
-  //Create
-  const onCreate = (title, content, category) => {
-    dispatch({
-      type: "CREATE",
-      data: {
-        id: dataId.current,
-        date: new Date().getTime(),
-        title,
-        content,
-        category,
-      },
-    });
-    dataId.current += 1;
-  };
-
-  //Edit
-  const onEdit = (targetId, date, title, content, category) => {
-    dispatch({
-      type: "EDIT",
-      data: {
-        id: targetId,
-        date: new Date(date).getTime(),
-        title,
-        content,
-        category,
-      },
-    });
-  };
-
-  //Remove
-  const onRemove = (targetId) => {
-    dispatch({
-      type: "REMOVE",
-      targetId,
-    });
-  };
 
   return (
     <BrowserRouter>
