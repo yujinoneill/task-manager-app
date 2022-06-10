b63ea533a407537ad52420b03dd6e78949ee2304
Oneill <yujinoneill@gmail.com>
2022-06-10 20:09:17 +0900

    CLN: Remove the files not used anymore

--- src/pages/wishlist/EditWish.js
+++
@@ -1,5 +0,0 @@
-const EditWish = () => {
-  return <div></div>;
-};
-
-export default EditWish;
--- src/pages/wishlist/NewWish.js
+++
@@ -1,7 +0,0 @@
-import Modal from "../../components/Modal";
-
-const NewWish = () => {
-  return <Modal />;
-};
-
-export default NewWish;
