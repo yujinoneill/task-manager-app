a25738083bd159d1b4480dac5fb4b3df8d8977b2
Oneill <yujinoneill@gmail.com>
2022-06-10 20:23:16 +0900

    FEAT: Add styles, Add funcs, Make WishBox that shows WishList data

--- src/components/wishlist/WishBox.js
+++ src/components/wishlist/WishBox.js
@@ -1,27 +1,124 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import styled from "styled-components";
+import { FaGrinHearts, FaWallet } from "react-icons/fa";
 
-import WishItem from "./WishItem";
+import Button from "../Button";
+import { wishActions } from "../../store/wishList";
+import Modal from "../Modal";
+import { DiaryBody, StyledBox } from "../diary/DiaryBox";
+import { getStringDate } from "../../util/date";
+import WishEditor from "./WishEditor";
 
-export const StyledDiv = styled.div`
-  padding-top: 20px;
+//Styled-components
+const Icon = styled.div`
+  color: white;
+  height: 80px;
+
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${(props) =>
+    props.icon === "Wish" ? "#274c77" : "#6096ba"};
+
+  svg {
+    font-size: 30px;
+  }
 `;
 
-const WishBox = (props) => {
+const StyledBody = styled(DiaryBody)`
+  background-color: ${(props) => props.icon === "Purchased" && "#e9ecef"};
+  line-height: 1.5;
+`;
+
+const WishBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  height: 40px;
+`;
+
+const WishBox = ({ id, date, icon, name, price, desc }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [originData, setOriginData] = useState();
+
+  const wishList = useSelector((state) => state.wish);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const targetWish = wishList.find(
+      (item) => parseInt(item.id) === parseInt(id)
+    );
+
+    if (targetWish) {
+      setOriginData(targetWish);
+    }
+  }, [id, wishList]);
+
+  const getHandler = (e) => {
+    if (window.confirm("Did you get this item?")) {
+      dispatch(
+        wishActions.wishEdit({
+          id,
+          date,
+          icon: "Purchased",
+          name,
+          price,
+          desc,
+        })
+      );
+    }
+    e.stopPropagation(); //onClick이 전파돼서 StyledBox의 modalHandler를 실행시키지 않도록
+  };
+
+  const modalHandler = () => {
+    setIsModalVisible(!isModalVisible);
+  };
+
+  const contentSlicer = (content) => {
+    if (content.length > 8) {
+      return content.slice(0, 8) + "...";
+    }
+    return content;
+  };
+
   return (
-    <StyledDiv>
-      <WishItem
-        icon={"wish"}
-        itemName={"Ball cap"}
-        price={"$35"}
-        itemDesc={"My style"}
-      />
-      {/* <WishItem
-        icon={"purchased"}
-        itemName={"IT Network Book"}
-        price={"$12"}
-        itemDesc={"Study Network"}
-      /> */}
-    </StyledDiv>
+    <StyledBox onClick={modalHandler}>
+      <Icon icon={icon}>
+        {icon === "Wish" ? <FaGrinHearts /> : <FaWallet />}
+      </Icon>
+      <StyledBody icon={icon}>
+        <h5>{name}</h5>
+        <p>{price}</p>
+        <p className="content">{contentSlicer(desc)}</p>
+        <hr />
+        <WishBottom>
+          <p className="post-date">{getStringDate(new Date(date))}</p>
+          {icon === "Wish" ? (
+            <Button name={"GET"} onClick={getHandler} />
+          ) : null}
+        </WishBottom>
+      </StyledBody>
+      {isModalVisible && (
+        <Modal
+          modalHandler={modalHandler}
+          children={
+            <WishEditor
+              originData={originData}
+              isEdit={true}
+              modalHandler={modalHandler}
+            />
+          }
+        />
+      )}
+    </StyledBox>
   );
 };
 
