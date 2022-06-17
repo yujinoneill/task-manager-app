6b01ce6e7bf6be71446d1dbdaddbcfe9ce2676d5
Oneill <yujinoneill@gmail.com>
2022-06-17 21:31:54 +0900

    FEAT: Add user reducer

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { diaryReducer } from "./diary";
 import { wishReducer } from "./wishList";
 import { todoReducer } from "./todoList";
+import { userReducer } from "./user";
 
 export const store = configureStore({
   reducer: {
     diary: diaryReducer.reducer,
     wish: wishReducer.reducer,
     todo: todoReducer.reducer,
+    user: userReducer.reducer,
   },
 });
 
