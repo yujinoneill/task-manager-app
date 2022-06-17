bd6c7e980763375397873b7b12e5d8bff7094f6b
Oneill <yujinoneill@gmail.com>
2022-06-17 21:41:18 +0900

    FEAT: Change the path of some files and Remove unnecessary style

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-
 import { FaSearch } from "react-icons/fa";
-import Dropdown from ".//Dropdown";
+
+import Dropdown from "../style/Dropdown";
 
 const StyledHeader = styled.header`
   display: flex;
