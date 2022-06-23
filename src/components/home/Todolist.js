e4a83ed620a8c31786fe8898b17ceb33a483978f
Oneill <yujinoneill@gmail.com>
2022-06-23 10:14:26 +0900

    initial commit

---
+++ src/components/home/Todolist.js
@@ -0,0 +1,154 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import styled from "styled-components";
+
+import { todoActions } from "../../store/todoList";
+import BasicButton from "../style/BasicButton";
+import CheckForm from "./CheckForm";
+
+//Styled-components
+const StyledDiv = styled.div`
+  padding-right: 5%;
+  border-right: 1px solid #e7ecef;
+
+  @media screen and (max-width: 992px) {
+    border-right: none;
+    border-bottom: 1px solid #e7ecef;
+
+    padding-right: 0;
+  }
+`;
+
+const Add = styled.div`
+  display: flex;
+  align-items: center;
+
+  input {
+    border: none;
+    border-bottom: 1px solid #6c757d;
+
+    padding: 10px 5px;
+
+    margin-right: 10px;
+
+    &:focus,
+    &:active {
+      border: none;
+      outline: none;
+      border-bottom: 1px solid #6c757d;
+    }
+  }
+`;
+
+const ProgressBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  padding: 20px 10px;
+
+  .progress-text {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Progress = styled.div`
+  width: 100%;
+  height: 20px;
+
+  margin: 10px 0;
+  border-radius: 10px;
+
+  background-color: #e9ecef;
+
+  .progress-bar {
+    width: ${(props) => props.width};
+    height: 100%;
+
+    background-color: #6096ba;
+    border-radius: 10px;
+
+    transition: width 1s;
+  }
+`;
+
+//Components
+const AddForm = React.memo(() => {
+  const [todo, setTodo] = useState("");
+  const dispatch = useDispatch();
+
+  //투두리스트 추가
+  const submitHandler = () => {
+    if (todo.length > 0) {
+      dispatch(
+        todoActions.todoCreate({
+          id: Math.random(),
+          checked: false,
+          todo,
+        })
+      );
+      setTodo("");
+    }
+  };
+
+  return (
+    <Add>
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <BasicButton name="Add a Todo" onClick={submitHandler} />
+    </Add>
+  );
+});
+
+const ProgressBar = React.memo(({ todoProgress }) => {
+  return (
+    <ProgressBox>
+      <div className="progress-text">
+        <span>Goals</span>
+        <span>{todoProgress}%</span>
+      </div>
+      <Progress width={`${todoProgress}%`}>
+        <div className="progress-bar"></div>
+      </Progress>
+    </ProgressBox>
+  );
+});
+
+const TodoList = () => {
+  //Progress Bar 계산
+  const [totalTodo, setTotalTodo] = useState();
+  const [completeTodo, setCompleteTodo] = useState();
+  const [todoProgress, setTodoProgress] = useState();
+
+  const todoList = useSelector((state) => state.todo);
+
+  const checkedTodo = todoList.filter((item) => item.checked === true);
+
+  useEffect(() => {
+    if (checkedTodo.length) {
+      setTotalTodo(todoList.length);
+      setCompleteTodo(checkedTodo.length);
+      setTodoProgress(
+        String(Math.floor((parseInt(completeTodo) / parseInt(totalTodo)) * 100))
+      );
+    } else {
+      setTodoProgress(0);
+    }
+  }, [checkedTodo]);
+
+  return (
+    <StyledDiv>
+      <AddForm />
+      {todoList.map((item) => (
+        <CheckForm key={item.id} {...item} />
+      ))}
+      <ProgressBar todoProgress={todoProgress} />
+    </StyledDiv>
+  );
+};
+
+export default React.memo(TodoList);
---
+++ src/pages/NotFound.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+import styled from "styled-components";
+import BasicButton from "../components/style/BasicButton";
+
+import { Background, LoginBox } from "./Login";
+
+const Content = styled(LoginBox)`
+  background-color: transparent;
+  border-radius: 0;
+  box-shadow: none;
+
+  p {
+    font-size: 20px;
+  }
+`;
+
+const NotFound = () => {
+  return (
+    <Background>
+      <Content>
+        <img
+          src={
+            process.env.PUBLIC_URL + "/assets/undraw_page_not_found_re_e9o6.svg"
+          }
+          alt="404-img"
+        />
+        <p>Ooooooooooooooooops!</p>
+        <Link to="/">
+          <BasicButton name="Go Back to Home" />
+        </Link>
+      </Content>
+    </Background>
+  );
+};
+
+export default NotFound;
